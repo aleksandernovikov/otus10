22d6c4f3b27d347b8e3c10618d4f61677a89ad8d
Alexander Novikov <79fz270704@gmail.com>
2020-05-02 11:43:10 +0300

    fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,27 @@
 import axios from 'axios'
 import Handlebars from 'handlebars'
+import {ENDPOINTS} from "../config/urls";
 
-function load_courses_data() {
-    axios.get('/api/courses')
+function loadCoursesData() {
+    axios.get(ENDPOINTS['coursesList'])
         .then(response => {
-            let src = `{{#each this}}<tr>
-                            <td>{{title}}</td>
-                            <td>{{#each teachers}}{{first_name}} {{last_name}}<br>{{/each}}</td>
-                            <td>{{start_date}}</td>
-                            <td>{{end_date}}</td>
-                            <td>{{#if finished}}Да{{else}}Нет{{/if}}</td>
-                        </tr>{{/each}}`
-            let t = Handlebars.compile(src)
-            document.querySelector('.courses-table').innerHTML = t(response.data)
+            let src = `{{#with this as | courses |}}
+                         {{#each courses}}
+                             <tr>
+                                <td>{{title}}</td>
+                                <td>{{#each teachers}}{{first_name}} {{last_name}}<br>{{/each}}</td>
+                                <td>{{start_date}}</td>
+                                <td>{{end_date}}</td>
+                                <td>{{#if finished}}Да{{else}}Нет{{/if}}</td>
+                            </tr>
+                         {{/each}}
+                       {{/with}}`
+            let template = Handlebars.compile(src)
+            document.querySelector('.courses-table').innerHTML = template(response.data)
         })
         .catch(error => {
             alert(error)
         })
 }
 
-load_courses_data()
+loadCoursesData();
