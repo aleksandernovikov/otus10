d1c69538c77d91f572508ed567b657cb514befd7
sanya <79fz270704@gmail.com>
2020-06-10 16:16:49 +0300

    fixes

--- frontend-react/src/components/loginForm/loginFormContainer.js
+++ frontend-react/src/components/loginForm/loginFormContainer.js
@@ -4,7 +4,7 @@ import {connect} from "react-redux"
 import LoginForm from './loginForm'
 
 import {getTokens} from "../../services/api";
-import {changeAuthStatus, changeLogin, changePassword} from "../../store/userData/actions";
+import {changeAuthStatus, changeDisplayName, changeLogin, changePassword} from "../../store/userData/actions";
 
 
 class LoginFormContainer extends React.Component {
@@ -22,6 +22,7 @@ const mapDispatchToProps = {
     loginClick: (login, password) => (dispatch) => {
         getTokens(login, password).then(userData => {
             if ('access' in userData && 'refresh' in userData) {
+                dispatch(changeDisplayName(localStorage.getItem('login')))
                 dispatch(changeAuthStatus(true))
             }
         }).catch(e => {
