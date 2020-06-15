d9f54cd4bebe196fc666293fc13bab1fe65afac2
sanya <79fz270704@gmail.com>
2020-06-15 16:42:59 +0300

    fixes

--- frontend-react/src/app.js
+++ frontend-react/src/app.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import {HashRouter as Router, Switch, Route} from "react-router-dom"
-import {Provider} from "react-redux";
-import {applyMiddleware, createStore} from "redux";
+import {Provider} from "react-redux"
+import {applyMiddleware, createStore} from "redux"
 
 import rootReducer from './store/reducers'
 import Home from './pages/homePage'
 import ViewCourse from './pages/courseDetailsPage'
 import thunk from "redux-thunk";
-import {composeWithDevTools} from "redux-devtools-extension";
-
+import {composeWithDevTools} from "redux-devtools-extension"
 
 const store = createStore(
     rootReducer,
